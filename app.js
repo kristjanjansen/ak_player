17d61da6b82d54714cb98710e048a11ad082a36a
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-02-13 14:21:47 +0200

    new take with clientside audio

--- app.js
+++ app.js
@@ -1,90 +1,41 @@
-var spawn = require('child_process').spawn
 var fs = require('fs');
 
-var arduino = require('duino')
 var tako = require('tako')
 var app = tako()
+var keypress = require('keypress')
+
+app.route('/music/*').files(__dirname + '/music');
+app.route('/').file(__dirname + '/client/index.html');
+app.route('/*').files(__dirname + '/client');
+
+app.httpServer.listen(8000)
 
 
-  app.route('/client/*').files(__dirname + '/client');
-  app.httpServer.listen(8000)
+var files = [
+  {files:['music/2.mp3']},
+  {files:['music/3.mp3']}
+]
 
-  
-  var f = fs.readdirSync('./music')
-  var i = 0
-  var delay = 500
+app.sockets.on('connection', function (socket) {
+  app.sockets.emit('queue', { files: files});
 
-  var player = {}
-  var playing = false
-
-  app.sockets.on('connection', function (socket) {
-    app.sockets.emit('message', { message: f[i]});
-  })
-
-  display(f[i])
-  
-  var board = new arduino.Board({
-  //  debug: true
-  });
-
-  sensor0 = new arduino.Sensor({
-    board: board,
-    pin: 'A0'
-  });
-
-  sensor1 = new arduino.Sensor({
-    board: board,
-    pin: 'A1'
-  });
-
-  sensor0.on('read', function(err, value) {
-    // Up
-    if (value > 890) {
-      if (i > 0) i--
-      display(f[i])
-      stopFile()
-      playFile(f[i])
-      board.delay(delay)
+  keypress(process.stdin);
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  process.stdin.on('keypress', function (ch, key) {
+    if (key && key.name == 'q') {
+      app.sockets.emit('play')        
+    } 
+    if (key && key.name == 'w') {
+      app.sockets.emit('prev')        
+    }  
+    if (key && key.name == 'e') {
+      app.sockets.emit('next')        
     }
-    // Down
-    if (value < 15) {
-      if (i < (f.length - 1)) i++
-      display(f[i])
-      stopFile()
-      playFile(f[i])
-      board.delay(delay)
+    if (key && key.ctrl && key.name == 'c') {
+      process.stdin.setRawMode(false);
+      process.stdin.pause();
     }
   });
 
-  sensor1.on('read', function(err, value) {
-  // Right
-  if (value < 15) {
-    display(f[i])
-    stopFile()
-    playFile(f[i])
-    board.delay(delay)  
-  }
-  // Left
-  if (value > 890) {
-    display(f[i])
-    stopFile()
-    board.delay(delay)  
-  }
-  });
-
-
-  function display(msg) {
-    app.sockets.emit('message', { message: msg});
-  }
-
-  function playFile(f) {
-    player = spawn('play', ['./music/' + f])      
-    playing = true
-  }
-
-  function stopFile() {
-    if (playing && player.pid) {
-      process.kill(player.pid)
-      playing = false
-    }
-  }
+})
--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,75 @@
-var socket = io.connect('http://localhost:8000');
+var socket = io.connect();
 
-socket.on('message', function (data) {
-  console.log(data);
-  $('#message').text(data.message)
+var files = []
+var current = 0
+var playing = false
+var howl = {}
+
+socket.on('queue', function (data) {
+  files = data.files
+  howl = new Howl({urls: files[current].files})
 });
+
+
+socket.on('play', function (data) {
+  if (!playing) {
+    howl.play()
+    playing = true 
+  } else {
+    howl.pause()
+    playing = false 
+  }
+});
+
+
+socket.on('next', function (data) {
+  console.log('next 1', current, files.length)
+  current = (current < files.length - 1) ? current + 1 : current
+  //current++
+  howl.urls(files[current].files)
+  console.log(howl.urls(), current)
+});
+
+socket.on('prev', function (data) {
+  current = (current > 0) ? current - 1 : current
+  howl.urls(files[current].files)
+  console.log(howl.urls())
+});
+
+
+/*
+var files = []
+
+socket.on('queue', function (data) {
+  data.files.forEach(function(item) {
+    var howl = new Howl({urls: [item.file]})
+    files.push({file: item.file, howl: howl})    
+  })
+});
+
+var current = 0
+
+socket.on('play', function (data) {
+  console.log('play', current)
+  files[current].howl.play()  
+});
+
+socket.on('stop', function (data) {
+  console.log('stop', current)
+  files[current].howl.stop()  
+});
+
+socket.on('next', function (data) {
+  files[current].howl.stop()
+  current = (current < files.length - 1) ? current++ : cu
+  console.log('next', current)  
+  files[current].howl.play()  
+});
+
+socket.on('prev', function (data) {
+  files[current].howl.stop()
+  current--
+  console.log('prev', current)  
+  files[current].howl.play()  
+});
+*/
